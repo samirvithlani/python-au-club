ab67d57695bcdc59815fbd21ccdeeb1fe0e89eda
samirvithlani <samir.vithlani83955@gmail.com>
2023-07-13 09:49:31 +0530

    ok

---
+++ oop/encp/inheritanceqcon.py
@@ -0,0 +1,18 @@
+class Student():
+    print("This is Student class")
+    
+    def __init__(self):
+        print("This is Student constructor")
+
+
+class StudentDetail(Student):
+    print("This is StudentDetail class")
+    
+    # def __init__(self):
+    #     print("This is StudentDetail constructor")
+    #     super().__init__()
+        
+
+
+#sd = StudentDetail()                
+        
